327782bcc1681a7e8d6f31fe40f83e7f0a76ded5
Trần Duy Toàn <93266912+tranduytoan@users.noreply.github.com>
2024-12-01 22:13:16 +0700

    chore: optimize fetching data

--- src/screens/admin/sub-pages/AuctionManagement.jsx
+++ src/screens/admin/sub-pages/AuctionManagement.jsx
@@ -8,6 +8,12 @@ import { AuctionStatus } from '../../../router';
 export const AuctionManagement = () => {
   const [activeMenuItem, setActiveMenuItem] = useState('All auctions');
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({
+    all: [],
+    open: [],
+    pending: [],
+  });
 
   const menuItems = [
     'All auctions',
@@ -36,21 +42,37 @@ export const AuctionManagement = () => {
     });
   };
 
+  const refreshData = async () => {
+    setLoading(true);
+    try {
+      const all = (await AdminService.searchAuctions({ status: '' })).data;
+      const open = (await AdminService.searchAuctions({ status: 'OPEN' })).data;
+      const pending = (await AdminService.searchAuctions({ status: 'PENDING' })).data;
+      setData({
+        all: formatAuctionData(all),
+        open: formatAuctionData(open),
+        pending: formatAuctionData(pending),
+      });
+      console.log('Data fetched:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   const handleChangeActiveMenuItem = async (activeMenuItem) => {
-    let filterStatus = '';
     switch (activeMenuItem) {
       case 'Open':
-        filterStatus = 'OPEN';
-        break;
+        setItems(data.open);
+        return;
       case 'Pending':
-        filterStatus = 'PENDING';
-        break;
+        setItems(data.pending);
+        return;
       default:
-        filterStatus = '';
-        break;
+        setItems(data.all);
+        return;
     }
-    const response = (await AdminService.searchAuctions({ status: filterStatus })).data;
-    setItems(formatAuctionData(response));
   }
 
   const sortOrders = (sortBy) => {
@@ -73,27 +95,28 @@ export const AuctionManagement = () => {
 
   useEffect(() => {
     handleChangeActiveMenuItem(activeMenuItem);
-  }, [activeMenuItem]);
+  }, [activeMenuItem, data]);
 
-  // useEffect(() => {
-  //   const fetchAuctions = async () => {
-  //     try {
-  //       const response = await AdminService.getAllAuctions(); // Gọi API để lấy tất cả đấu giá
-  //       setItems(formatAuctionData(response.data));
-  //     } catch (error) {
-  //       console.error("Error fetching auctions:", error);
-  //     }
-  //   };  
-  //   fetchAuctions();
-  // }, []);
+  useEffect(() => {
+    refreshData();
+  }, []);
 
   return (
     <div className="flex">
       <Sidebar menuItems={menuItems} activeMenuItem={activeMenuItem} setActiveMenuItem={setActiveMenuItem} />
-      <div className="w-5/6 p-6">
-        <h1 className="text-2xl mb-4">{ header(activeMenuItem) }</h1>
-        <FilterBar />
-        <Table items={items} sortOptions={sortOptions} sortFuntion={sortOrders} />
+      <div className="w-5/6 p-6 relative">
+        <h1 className="text-2xl mb-4 inline-block">{ header(activeMenuItem) }</h1>
+        <button className='ml-4 align-middle' onClick={refreshData} title='Refresh'>
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12C21 16.9706 16.9706 21 12 21C9.69494 21 7.59227 20.1334 6 18.7083L3 16M3 12C3 7.02944 7.02944 3 12 3C14.3051 3 16.4077 3.86656 18 5.29168L21 8M3 21V16M3 16H8M21 3V8M21 8H16" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+        </button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <FilterBar />
+            <Table items={items} sortOptions={sortOptions} sortFunction={sortOrders} />
+          </>
+        )}
       </div>
     </div>
   );
--- src/screens/admin/sub-pages/UserManagement.jsx
+++ src/screens/admin/sub-pages/UserManagement.jsx
@@ -8,6 +8,14 @@ import { Role } from '../../../router/index';
 export const UserManagement = () => {
   const [items, setItems] = useState([]);
   const [activeMenuItem, setActiveMenuItem] = useState('All users');
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({
+    all: [],
+    sellers: [],
+    bidders: [],
+    inactive: [],
+    banned: [],
+  });
 
   const menuItems = [
     'All users',
@@ -44,7 +52,7 @@ export const UserManagement = () => {
   }
 
   const formatUserData = (response) => {
-    return response.content.map(user => {
+    return response.map(user => {
       return {
         username: user.profile && user.profile.fullName ? user.profile.fullName : 'Unknown',
         email: user.email,
@@ -55,46 +63,74 @@ export const UserManagement = () => {
     });
   }
 
+  const refreshData = async () => {
+    setLoading(true);
+    try {
+      const all = (await AdminService.searchUsers({ role: '', isVerified: '', isBanned: '' })).data.content;
+      const sellers = all.filter(user => user.role === 'SELLER');
+      const bidders = all.filter(user => user.role === 'BIDDER');
+      const inactive = all.filter(user => !user.verified);
+      const banned = all.filter(user => user.banned);
+
+      setData({
+        all: formatUserData(all),
+        sellers: formatUserData(sellers),
+        bidders: formatUserData(bidders),
+        inactive: formatUserData(inactive),
+        banned: formatUserData(banned),
+      });
+      console.log('Data fetched:', data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   const handleChangeActiveMenuItem = async (activeMenuItem) => {
-    let filterRole = '';
-    let filterVerify = '';
-    let filterBanned = '';
     switch (activeMenuItem) {
       case 'Sellers':
-          filterRole = 'SELLER';
-          break;
+        setItems(data.sellers);
+        return;
       case 'Bidders':
-          filterRole = 'BIDDER';
-          break;
+        setItems(data.bidders);
+        return;
       case 'Inactive':
-          filterVerify = false;
-          break;
+        setItems(data.inactive);
+        return;
       case 'Banned':
-          filterBanned = true;
-          break;
+        setItems(data.banned);
+        return;
       default:
-          break;
+        setItems(data.all);
+        return;
     }
-    const response = await (await AdminService.searchUsers({ role: filterRole, isVerified: filterVerify, isBanned: filterBanned })).data
-    console.log("response", response);
-    setItems(formatUserData(response));
   }
 
   useEffect(() => {
-    try {
-      handleChangeActiveMenuItem(activeMenuItem);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [activeMenuItem]);
+    handleChangeActiveMenuItem(activeMenuItem);
+  }, [activeMenuItem, data]);
+
+  useEffect(() => {
+    refreshData();
+  }, []);
 
   return (
     <div className="flex">
       <Sidebar menuItems={menuItems} activeMenuItem={activeMenuItem} setActiveMenuItem={setActiveMenuItem} />
-      <div className="w-5/6 p-6">
-        <h1 className="text-2xl mb-4">{ header(activeMenuItem) }</h1>
-        <FilterBar />
-        <Table items={items} sortOptions={sortOptions} sortFuntion={sortOrders} />
+      <div className="w-5/6 p-6 relative">
+        <h1 className="text-2xl mb-4 inline-block">{ header(activeMenuItem) }</h1>
+        <button className='ml-4 align-middle' onClick={refreshData} title='Refresh'>
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12C21 16.9706 16.9706 21 12 21C9.69494 21 7.59227 20.1334 6 18.7083L3 16M3 12C3 7.02944 7.02944 3 12 3C14.3051 3 16.4077 3.86656 18 5.29168L21 8M3 21V16M3 16H8M21 3V8M21 8H16" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
+        </button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <FilterBar />
+            <Table items={items} sortOptions={sortOptions} sortFunction={sortOrders} />
+          </>
+        )}
       </div>
     </div>
   );
