939ea9b6871f61cc9cb9e8ba6edad0804d0396bb
Trần Duy Toàn <93266912+tranduytoan@users.noreply.github.com>
2024-12-19 02:04:23 +0700

    chore: remove videos feature

--- src/screens/seller-hub/components/PhotoUpload.jsx
+++ src/screens/seller-hub/components/PhotoUpload.jsx
@@ -22,17 +22,17 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
   );
 
   // Video Upload Handlers
-  const handleVideoUpload = useCallback(
-    (event) => {
-      if (disabled) return; // Vô hiệu hóa thao tác
-      const uploadedVideos = Array.from(event.target.files);
-      setProductDetails((prevDetails) => ({
-        ...prevDetails,
-        videos: [...videos, ...uploadedVideos],
-      }));
-    },
-    [videos, setProductDetails, disabled]
-  );
+  // const handleVideoUpload = useCallback(
+  //   (event) => {
+  //     if (disabled) return; // Vô hiệu hóa thao tác
+  //     const uploadedVideos = Array.from(event.target.files);
+  //     setProductDetails((prevDetails) => ({
+  //       ...prevDetails,
+  //       videos: [...videos, ...uploadedVideos],
+  //     }));
+  //   },
+  //   [videos, setProductDetails, disabled]
+  // );
 
   // Select Primary Photo
   const handleSelectPrimary = useCallback(
@@ -61,14 +61,14 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
     [photos, primaryIndex, setProductDetails, disabled]
   );
 
-  const handleVideoDelete = useCallback(
-    (index) => {
-      if (disabled) return; // Vô hiệu hóa thao tác
-      const updatedVideos = videos.filter((_, videoIndex) => videoIndex !== index);
-      setProductDetails({ ...productDetails, videos: updatedVideos });
-    },
-    [videos, setProductDetails, productDetails, disabled]
-  );
+  // const handleVideoDelete = useCallback(
+  //   (index) => {
+  //     if (disabled) return; // Vô hiệu hóa thao tác
+  //     const updatedVideos = videos.filter((_, videoIndex) => videoIndex !== index);
+  //     setProductDetails({ ...productDetails, videos: updatedVideos });
+  //   },
+  //   [videos, setProductDetails, productDetails, disabled]
+  // );
 
   // Drag Handlers
   const handlePhotoDragOver = (event) => {
@@ -88,30 +88,30 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
     }));
   };
 
-  const handleVideoDragOver = (event) => {
-    if (disabled) return; // Vô hiệu hóa thao tác
-    event.preventDefault();
-  };
+  // const handleVideoDragOver = (event) => {
+  //   if (disabled) return; // Vô hiệu hóa thao tác
+  //   event.preventDefault();
+  // };
 
-  const handleVideoDrop = (event) => {
-    if (disabled) return; // Vô hiệu hóa thao tác
-    event.preventDefault();
-    const uploadedVideos = Array.from(event.dataTransfer.files).filter((file) =>
-      file.type.startsWith('video/')
-    );
-    setProductDetails((prevDetails) => ({
-      ...prevDetails,
-      videos: [...videos, ...uploadedVideos],
-    }));
-  };
+  // const handleVideoDrop = (event) => {
+  //   if (disabled) return; // Vô hiệu hóa thao tác
+  //   event.preventDefault();
+  //   const uploadedVideos = Array.from(event.dataTransfer.files).filter((file) =>
+  //     file.type.startsWith('video/')
+  //   );
+  //   setProductDetails((prevDetails) => ({
+  //     ...prevDetails,
+  //     videos: [...videos, ...uploadedVideos],
+  //   }));
+  // };
 
   return (
     <div className="mb-8">
-      <h2 className="text-lg font-semibold mb-2">PHOTO & VIDEO</h2>
+      <h2 className="text-lg font-semibold mb-2">PHOTOS</h2>
       <div className="flex space-x-4">
         {/* Photo Upload Area */}
         <div
-          className={`w-3/5 h-72 border border-dashed rounded-xl flex items-center justify-center flex-wrap gap-4 p-2 ${
+          className={`w-full h-72 border border-dashed rounded-xl flex items-center justify-center flex-wrap gap-4 p-2 ${
             disabled ? 'opacity-50 pointer-events-none' : ''
           }`}
           onDragOver={handlePhotoDragOver}
@@ -135,7 +135,7 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
               {photos.map((photo, index) => (
                 <div
                   key={index}
-                  className={`relative w-20 h-20 border rounded overflow-hidden ${
+                  className={`relative w-32 h-32 border rounded overflow-hidden ${
                     index === primaryIndex ? 'border-green-500 border-2' : ''
                   }`}
                 >
@@ -184,7 +184,7 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
         </div>
 
         {/* Video Upload Area */}
-        <div
+        {/* <div
           className={`w-2/5 h-72 border border-dashed rounded-xl flex items-center justify-center flex-wrap gap-4 p-2 ${
             disabled ? 'opacity-50 pointer-events-none' : ''
           }`}
@@ -239,7 +239,7 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
               )}
             </>
           )}
-        </div>
+        </div> */}
       </div>
 
       {/* Photo Preview Modal */}
@@ -258,7 +258,7 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
       )}
 
       {/* Video Preview Modal */}
-      {previewVideo && (
+      {/* {previewVideo && (
         <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
           <div className="relative bg-white p-0 rounded-lg shadow-lg max-w-4xl">
             <video controls src={previewVideo} className="max-w-full max-h-[800px] rounded-md"></video>
@@ -270,7 +270,7 @@ export const PhotoUpload = memo(({ productDetails, setProductDetails, disabled =
             </button>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 });
